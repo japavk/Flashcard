89c131e2c570f7be996edab2253e885a7dddeab5
japa <00001104474980sp@al.educacao.sp.gov.br>
2024-11-24 23:23:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,68 @@
+criaCartao(
+    'Fortnite',
+    'qual é o max de material no battle royale?',
+    '500 de cada material'
+)
+
+criaCartao(
+    'Fortnite',
+    'quanto de shield vc fica se tomar um shildão e depois tomar um mini?',
+    '50'
+)
+
+criaCartao(
+    'Anime',
+    'Quem é mais forte o goku ou o saitama?',
+    'Goku'
+)
+
+criaCartao(
+    'Valorant',
+    'Quem ganhou a champions em 2021?',
+    'Loud'
+)
+criaCartao(
+    'Valorant',
+    'Quanto custa a vandal?',
+    '2.900'
+)
+criaCartao(
+    'Famosos',
+    'Quem tinha mais de 1000 frascos de oleo de bebê em casa?',
+    'Pi Diddy'
+)
+criaCartao(
+    'Series',
+    'Quem é que morre na ultima temporada de cobra kai?',
+    'Kwon'
+)
+criaCartao(
+    'Series',
+    'Quem são supostamentes os dois melhores lutadores da ultima temporada de cobra kai?',
+    'Axel Kovačević e Zara Malik'
+)
+criaCartao(
+    'Series',
+    'Qual é o verdadeiro nome da Jinx?',
+    'Powder'
+)
+criaCartao(
+    'Series',
+    'Quem criou o hextech?',
+    'Jayce e Viktor'
+)
+criaCartao(
+    'Series',
+    'Quem criou a Jinx depois que a Vi abandonou ela?',
+    'Silco'
+)
+criaCartao(
+    'Series',
+    'Qual nome completo da Vi?',
+    'Violet'
+)
+criaCartao(
+    'Famosos',
+    'Quem yuri22 venera?',
+    'Babi Romiti'
+)
